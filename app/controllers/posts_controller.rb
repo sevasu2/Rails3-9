8c0bd7dcfd05732fd154a450e81ff8551bb7c259
sevasu2 <sevasunishimura@gmail.com>
2018-09-05 10:38:16 +0000

    [Finish]3-6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,21 @@ class PostsController < ApplicationController
   	
   end
 
+  def index
+  	@posts = Post.all
+  end
+
+  def show
+  	@post = Post.find(params[:id]) 
+
+  end
+
+  def create
+        post = Post.new(post_params)
+        post.save # DBへ保存する
+        redirect_to post_path(post.id) # 詳細画面へリダイレクト    end
+  end
+
   private
 
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   get '/top' => 'root#top'
 
   post '/posts' => 'posts#create'
+
+  get '/posts' => 'posts#index'
+
+  get '/posts/:id' => 'posts#show' , as: 'post'
 end
